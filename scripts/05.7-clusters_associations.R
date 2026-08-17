d7ae64c9e7b4d3ac8ec91bdff2c7dfdfa850c696
EmelineFavreau <emeline.a.favreau@gmail.com>
2026-08-17 15:28:04 +0100

    updating typo in Fig 5

--- scripts/05.7-clusters_associations.R
+++ scripts/05.7-clusters_associations.R
@@ -2,43 +2,40 @@
 source("common.R")
 
 ######################## import input ##########################################
-# presence/absence matrix
-phbm <- readRDS("../result/patient_hpo_bio_mat.RDS")
 
-# HPO codes
+patient_hpo_bio_mat <- readRDS("../result/patient_hpo_bio_mat.RDS")
 unique_codes <- readRDS("../result/unique_codes.RDS")
 
-# present HPO codes
-hpo_vec <- rownames(phbm)[grep("^HP", rownames(phbm))]
+hpo_vec <- rownames(patient_hpo_bio_mat)[grep("^HP",
+                                              rownames(patient_hpo_bio_mat))]
 
-# present Key HPO groups
-key_hpo_vec <- rownames(phbm)[grep("^key", rownames(phbm))]
+key_hpo_vec <- rownames(patient_hpo_bio_mat)[grep("^key",
+                                              rownames(patient_hpo_bio_mat))]
+####### Hypothesis D: Association between patient cluster and biology ##########
+# Hypothesis D underlying all those tests
+# e.g. clustering patients in groups are associated with a given phenotype or genotype
 
-######################## association tests #####################################
-# association between infection/conplex and biomarkers
-
- 
 # result p values list
-p_list <- list()
-c_list <- list()
-s_list <- list()
+pvalue_cluster_result_list <- list()
+contingency_cluster_result_list <- list()
+summary_contingency_cluster_result_list <- list()
 
 ##### PART 1: B cell
 # setting parameters
-bn <- c("smb", "cd21", "tr")
+biological_names <- c("smb", "cd21", "tr")
 
-bc <- c("smb_minus", "smb_normal",
+biological_cat <- c("smb_minus", "smb_normal",
                     "cd21_low", "cd21_plus",
                     "tr_norm", "tr_high")
 
-names(bc) <- c(rep(bn[1:3], each = 2))
+names(biological_cat) <- c(rep(biological_names[1:3], each = 2))
 
 # looping through biological categories
-for(k in 1:length(bn)){
+for(k in 1:length(biological_names)){
   
   # select measure
-  fn <- bn[k]
-  fc <- bc[names(bc) == fn]
+  focus_name <- biological_names[k]
+  focus_cat <- biological_cat[names(biological_cat) == focus_name]
   
   # initiate matrix
   mat_long <- matrix(data = 0,
@@ -47,13 +44,13 @@ for(k in 1:length(bn)){
   # loop through the centres
   for(c in names(centres_vec)){
     # loop through the B cell
-    for(b in fc){
+    for(b in focus_cat){
       # loop through the cluster membership
       for(g in c("infection_cluster", "complex_cluster")){
         # collect the categories
         i <- c(b, g, c) 
         # subset the data to those categories
-        id <- phbm[row.names(phbm) %in% i, ]
+        id <- patient_hpo_bio_mat[row.names(patient_hpo_bio_mat) %in% i, ]
         # count the number of patients in this centre and this cluster
         s <- sum(colSums(id) == 3) 
         mat_long <- rbind(mat_long, c(i,s))
@@ -72,7 +69,7 @@ for(k in 1:length(bn)){
   }
   
   # name the columns
-  colnames(mat_long) <- c(fn, "phenotype", "centre", "count")
+  colnames(mat_long) <- c(focus_name, "phenotype", "centre", "count")
   
   # make it into a dataframe
   mat_long <- as.data.frame(mat_long)
@@ -80,55 +77,58 @@ for(k in 1:length(bn)){
   
   # make a three-dimensional table
   contingency_table <- xtabs(
-    as.formula(paste("count ~ phenotype +", fn, "+ centre")),
+    as.formula(paste("count ~ phenotype +", focus_name, "+ centre")),
     data = mat_long
   )
   contingency_table1 <- xtabs(
-    as.formula(paste("count ~ phenotype +", fn)),
+    as.formula(paste("count ~ phenotype +", focus_name)),
     data = mat_long
   )
   
   # test for association, save p value
   cluster_result <- mantelhaen.test(contingency_table)
   pvec <- cluster_result$p.value
-  thisname <- paste(fn, "_cluster", sep = "")
+  thisname <- paste(focus_name, "_cluster", sep = "")
   names(pvec) <- thisname
-  c_list[[thisname]] <- contingency_table
-  s_list[[thisname]] <- contingency_table1
+  contingency_cluster_result_list[[thisname]] <- contingency_table
+  summary_contingency_cluster_result_list[[thisname]] <- contingency_table1
   
   # some results are NaN, because the contingency tables are so imbalanced
-  p_list[[k]] <- pvec[!is.nan(pvec)]
-  names(p_list)[k] <- fn
+  pvalue_cluster_result_list[[k]] <- pvec[!is.nan(pvec)]
+  names(pvalue_cluster_result_list)[k] <- focus_name
 }
 
 
 # PART 2: Ig and genetics and key HPO groups
 # setting parameters
-genetic <- c("canonicalTNFRSF13B",       
-              "rareTNFRSF13B",
-              "NFKB1",
-              "anyOtherPathogenic",
-              "anyPathogenic")
-bn <- c("lowIgGlowIgAhighIgM",
-        "lowIgGlowIgAnormalIgM", 
-        "lowIgGlowIgAlowIgM",
-        "lowIgGnormalIgAlowIgM",
-        genetic, key_hpo_vec)
+biological_names <- c("lowIgGlowIgAhighIgM",
+                      "lowIgGlowIgAnormalIgM", 
+                      "lowIgGlowIgAlowIgM",
+                      "lowIgGnormalIgAlowIgM",
+                      "canonicalTNFRSF13B",
+                      "rareTNFRSF13B",
+                      "NFKB1", 
+                      "anyPathogenic",
+                      key_hpo_vec)
 
-bc <- c("llh",
-        "lln",
-        "lll",
-        "lnl",
-        genetic, key_hpo_vec)
+biological_cat <- c("llh",
+                    "lln",
+                    "lll",
+                    "lnl",
+                    "canonicalTNFRSF13B",
+                    "rareTNFRSF13B",
+                    "NFKB1",
+                    "anyPathogenic",
+                    key_hpo_vec)
 
-names(bc) <- bn
+names(biological_cat) <- biological_names
 
 
 # looping through biological categories
-for(k in 1:length(bn)){
+for(k in 1:length(biological_names)){
   # select measure
-  fn <- bn[k]
-  fc <- bc[names(bc) == fn]
+  focus_name <- biological_names[k]
+  focus_cat <- biological_cat[names(biological_cat) == focus_name]
   
   # initiate matrix
   mat_long <- matrix(data = 0,
@@ -139,18 +139,18 @@ for(k in 1:length(bn)){
     # loop through the cluster membership
     for(g in c("infection_cluster", "complex_cluster")){
       # collect the categories
-      i <- c(fc, g, c) 
+      i <- c(focus_cat, g, c) 
       # subset the data to those categories
-      id <- phbm[row.names(phbm) %in% i, ]
+      id <- patient_hpo_bio_mat[row.names(patient_hpo_bio_mat) %in% i, ]
       # count the number of patients of that centre in the cluster and with the bio
       s <- sum(colSums(id) == 3) 
       mat_long <- rbind(mat_long, c(i,s))
       # count the number of patients of that centre in the cluster and without the bio
-      s1 <- sum(id[fc, ] == 0 &
+      s1 <- sum(id[focus_cat, ] == 0 &
                   id[c, ] == 1 &
                   id[g, ] == 1)
       mat_long <- rbind(mat_long,
-                        c(paste("no", fc, sep =""),
+                        c(paste("no", focus_cat, sep =""),
                           i[2:3],
                           s1))
       
@@ -168,7 +168,7 @@ for(k in 1:length(bn)){
   }
   
   # name the columns
-  colnames(mat_long) <- c(fn, "phenotype", "centre", "count")
+  colnames(mat_long) <- c(focus_name, "phenotype", "centre", "count")
   
   # make it into a dataframe
   mat_long <- as.data.frame(mat_long)
@@ -179,16 +179,17 @@ for(k in 1:length(bn)){
   for(c in names(centres_vec)){
     if(sum(mat_long[,4][mat_long[,3] == c][1:2] == 0) == 2 |
        sum(mat_long[,4][mat_long[,3] == c][3:4] == 0) == 2){
+      #print("the patients in this centre present no variation for this hpo") 
       mat_long <- mat_long[mat_long[,3] !=c, ]}
   }
   
   # make a three-dimensional table
   contingency_table <- xtabs(
-    as.formula(paste("count ~ phenotype +", fn, "+ centre")),
+    as.formula(paste("count ~ phenotype +", focus_name, "+ centre")),
     data = mat_long
   )
   contingency_table1 <- xtabs(
-    as.formula(paste("count ~ phenotype +", fn)),
+    as.formula(paste("count ~ phenotype +", focus_name)),
     data = mat_long
   )
   
@@ -196,28 +197,27 @@ for(k in 1:length(bn)){
   # test for association, save p value
   cluster_result <- mantelhaen.test(contingency_table)
   pvec <- cluster_result$p.value
-  thisname <- paste(fn, "_cluster", sep = "")
+  thisname <- paste(focus_name, "_cluster", sep = "")
   names(pvec) <- thisname
-  c_list[[thisname]] <- contingency_table
-  s_list[[thisname]] <- contingency_table1
+  contingency_cluster_result_list[[thisname]] <- contingency_table
+  summary_contingency_cluster_result_list[[thisname]] <- contingency_table1
   
   # some results are NaN, because the contingency tables are so imbalanced
   # B cell occupies 1 to 3, so add
   result_row <- 3
-  p_list[[k+result_row]] <- pvec[!is.nan(pvec)]
-  names(p_list)[k+result_row] <- fn
+  pvalue_cluster_result_list[[k+result_row]] <- pvec[!is.nan(pvec)]
+  names(pvalue_cluster_result_list)[k+result_row] <- focus_name
   
 }
 
 
 # adjustment for multiple comparison Benjamini and Hochberg
-BH_cluster_pvalue_result <- p.adjust(unlist(p_list),
+BH_cluster_pvalue_result <- p.adjust(unlist(pvalue_cluster_result_list),
                                      method = "BH")
 biological_measure <- gsub("\\..*$",
-             names(BH_cluster_pvalue_result[BH_cluster_pvalue_result < 0.05]),
-             replacement = "")
-BH_adjust_pvalue <- 
-  unname(BH_cluster_pvalue_result[BH_cluster_pvalue_result < 0.05])
+                           names(BH_cluster_pvalue_result[BH_cluster_pvalue_result < 0.05]),
+                           replacement = "")
+BH_adjust_pvalue <- unname(BH_cluster_pvalue_result[BH_cluster_pvalue_result < 0.05])
 
 BH_cluster_CHM_table <- data.frame(
   biological_measure,BH_adjust_pvalue)
@@ -227,10 +227,14 @@ BH_cluster_CHM_table$BH_adjust_pvalue <-
   formatC(BH_cluster_CHM_table$BH_adjust_pvalue,
           format = "g")
 
+
+# save table
+fwrite(BH_cluster_CHM_table , "../result/bio_cluster_tests.csv")
+
 # qualify result
 c1 <- matrix(NA, ncol = 3)
 df <- BH_cluster_CHM_table
-rl <- s_list
+rl <- summary_contingency_cluster_result_list
 for(i in 1:nrow(df)){
   thisname <- paste(df[i, 1],
                     "cluster", sep = "_")
@@ -240,32 +244,39 @@ for(i in 1:nrow(df)){
 }
 c1 <- c1[complete.cases(c1), ]
 colnames(c1) <- c("cluster",
-                  "biological_category",
+                  "biological_measure",
                   "patient_count")
 
 # find the num of patients in each biological group
-bg <- unique(c1$biological_category)
+bg <- unique(c1$biological_measure)
 jj <- c()
 for (i in 1:length(bg)){
-  j <- sum(phbm[rownames(phbm) == bg[i], ])
+  j <- sum(patient_hpo_bio_mat[rownames(patient_hpo_bio_mat) == bg[i], ])
   jj <- c(jj, j)
 }
 names(jj) <- bg
 
 c1$total_patient_in_biological_measure <- 
-  jj[match(c1$biological_category, names(jj) )]
+  jj[match(c1$biological_measure, names(jj) )]
 
 c1$percentage_in_biological_measure <- 
   (c1$patient_count/c1$total_patient_in_biological_measure)*100
 
-c1$biological_measure <- ifelse(grepl("_", c1$biological_category), 
-            gsub(pattern = "_.*", c1$biological_category, replacement = ""),
-            gsub(pattern = "^no", c1$biological_category, replacement = ""))
 
-# merge info
-merged_tibble <- c1 %>%
-  full_join(BH_cluster_CHM_table, by = "biological_measure")
+# save
+fwrite(c1,
+       "../result/cluster_HPO_tests_summary_contingency_with_ratio.csv")
 
-############## save all
-fwrite(merged_tibble, "../result/cluster_biomarker_tests_ratio_pvalue.csv")
+# cols:cluster,biological_category,patient_count,
+# total_patient_in_biological_measure,percentage_in_biological_measure,
+# biological_measure,BH_adjust_pvalue
+# combine c1 and BH_cluster_CHM_table
+c1$biological_category <- c1$biological_measure %>%
+  gsub(pattern = "_.*$", replacement = "", perl = TRUE)
+c1$BH_adjust_pvalue <- 
+  BH_cluster_CHM_table$BH_adjust_pvalue[match(c1$biological_category,
+                                    BH_cluster_CHM_table$biological_measure)]
 
+# save
+fwrite(c1,
+      "../result/cluster_biomarker_tests_ratio_pvalue.csv") 
